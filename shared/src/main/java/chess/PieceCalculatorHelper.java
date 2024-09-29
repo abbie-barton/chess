61cae925369cd4a5be4eb360781847026bf2ac9a
Abbie Barton <abbie.s.barton@gmail.com>
2024-09-29 17:16:02 -0600

    add test files and clean up addMovesInDirection function

--- shared/src/main/java/chess/PieceCalculatorHelper.java
+++ shared/src/main/java/chess/PieceCalculatorHelper.java
@@ -38,10 +38,10 @@ public class PieceCalculatorHelper {
         Collection<ChessMove> moveList = new ArrayList<ChessMove>();
 
         // Add moves for each direction for the bishop
-        addMovesInDirection(moveList, myPosition, board, 1, 1);   // Diagonal up-right
-        addMovesInDirection(moveList, myPosition, board, 1, -1);  // Diagonal up-left
-        addMovesInDirection(moveList, myPosition, board, -1, -1); // Diagonal down-left
-        addMovesInDirection(moveList, myPosition, board, -1, 1);  // Diagonal down-right
+        addMovesInDirection(moveList, myPosition, board, 1, 1, false);   // Diagonal up-right
+        addMovesInDirection(moveList, myPosition, board, 1, -1, false);  // Diagonal up-left
+        addMovesInDirection(moveList, myPosition, board, -1, -1, false); // Diagonal down-left
+        addMovesInDirection(moveList, myPosition, board, -1, 1, false);  // Diagonal down-right
 
         return moveList;
     }
@@ -49,10 +49,10 @@ public class PieceCalculatorHelper {
     public static Collection<ChessMove> rookCalculator(ChessPosition myPosition, ChessBoard board) {
         Collection<ChessMove> moveList = new ArrayList<ChessMove>();
 
-        addMovesInDirection(moveList, myPosition, board, 1, 0);   // up
-        addMovesInDirection(moveList, myPosition, board, -1, 0);  // down
-        addMovesInDirection(moveList, myPosition, board, 0, -1); // left
-        addMovesInDirection(moveList, myPosition, board, 0, 1);  // right
+        addMovesInDirection(moveList, myPosition, board, 1, 0, false);   // up
+        addMovesInDirection(moveList, myPosition, board, -1, 0, false);  // down
+        addMovesInDirection(moveList, myPosition, board, 0, -1, false); // left
+        addMovesInDirection(moveList, myPosition, board, 0, 1, false);  // right
 
         return moveList;
     }
@@ -69,14 +69,14 @@ public class PieceCalculatorHelper {
     public static Collection<ChessMove> knightCalculator(ChessPosition myPosition, ChessBoard board) {
         Collection<ChessMove> moveList = new ArrayList<ChessMove>();
 
-        addLimitedMovesInDirection(moveList, myPosition, board, 2, 1);
-        addLimitedMovesInDirection(moveList, myPosition, board, 2, -1);
-        addLimitedMovesInDirection(moveList, myPosition, board, -2, 1);
-        addLimitedMovesInDirection(moveList, myPosition, board, -2, -1);
-        addLimitedMovesInDirection(moveList, myPosition, board, 1, 2);
-        addLimitedMovesInDirection(moveList, myPosition, board, 1, -2);
-        addLimitedMovesInDirection(moveList, myPosition, board, -1, 2);
-        addLimitedMovesInDirection(moveList, myPosition, board, -1, -2);
+        addMovesInDirection(moveList, myPosition, board, 2, 1, true);
+        addMovesInDirection(moveList, myPosition, board, 2, -1, true);
+        addMovesInDirection(moveList, myPosition, board, -2, 1, true);
+        addMovesInDirection(moveList, myPosition, board, -2, -1, true);
+        addMovesInDirection(moveList, myPosition, board, 1, 2, true);
+        addMovesInDirection(moveList, myPosition, board, 1, -2, true);
+        addMovesInDirection(moveList, myPosition, board, -1, 2, true);
+        addMovesInDirection(moveList, myPosition, board, -1, -2, true);
 
         return moveList;
     }
@@ -84,19 +84,19 @@ public class PieceCalculatorHelper {
     public static Collection<ChessMove> kingCalculator(ChessPosition myPosition, ChessBoard board) {
         Collection<ChessMove> moveList = new ArrayList<ChessMove>();
 
-        addLimitedMovesInDirection(moveList, myPosition, board, 1, 1);
-        addLimitedMovesInDirection(moveList, myPosition, board, 1, -1);
-        addLimitedMovesInDirection(moveList, myPosition, board, 1, 0);
-        addLimitedMovesInDirection(moveList, myPosition, board, -1, 1);
-        addLimitedMovesInDirection(moveList, myPosition, board, -1, -1);
-        addLimitedMovesInDirection(moveList, myPosition, board, -1, 0);
-        addLimitedMovesInDirection(moveList, myPosition, board, 0, 1);
-        addLimitedMovesInDirection(moveList, myPosition, board, 0, -1);
+        addMovesInDirection(moveList, myPosition, board, 1, 1, true);
+        addMovesInDirection(moveList, myPosition, board, 1, -1, true);
+        addMovesInDirection(moveList, myPosition, board, 1, 0, true);
+        addMovesInDirection(moveList, myPosition, board, -1, 1, true);
+        addMovesInDirection(moveList, myPosition, board, -1, -1, true);
+        addMovesInDirection(moveList, myPosition, board, -1, 0, true);
+        addMovesInDirection(moveList, myPosition, board, 0, 1, true);
+        addMovesInDirection(moveList, myPosition, board, 0, -1, true);
 
         return moveList;
     }
 
-    private static void addMovesInDirection(Collection<ChessMove> moveList, ChessPosition myPosition, ChessBoard board, int rowIncrement, int colIncrement) {
+    private static void addMovesInDirection(Collection<ChessMove> moveList, ChessPosition myPosition, ChessBoard board, int rowIncrement, int colIncrement, boolean limited) {
         int row = myPosition.getRow();
         int col = myPosition.getColumn();
 
@@ -122,28 +122,10 @@ public class PieceCalculatorHelper {
             } else {
                 isValid = 0;
             }
-            counter += 1;
-        }
-    }
-
-    private static void addLimitedMovesInDirection(Collection<ChessMove> moveList, ChessPosition myPosition, ChessBoard board, int rowIncrement, int colIncrement) {
-        // go forward only one time (ex for knight and king)
-        int row = myPosition.getRow();
-        int col = myPosition.getColumn();
-
-        int newRow = row + rowIncrement;
-        int newCol = col + colIncrement;
-
-        // Check if the new position is within bounds and if a piece is not blocking the way
-        if ((newRow >= 1 && newRow <= 8 && newCol >= 1 && newCol <= 8)) {
-            ChessPiece newPiece = board.getPiece(new ChessPosition(newRow, newCol));
-            // logic to add a possible move to capture a piece of the opposing color, but go no farther
-            if (newPiece != null && board.getPiece(myPosition).getTeamColor() != newPiece.getTeamColor()) {
-                moveList.add(new ChessMove(myPosition, new ChessPosition(newRow, newCol), null));
-                // if there is no piece at that position, add a move
-            } else if (newPiece == null) {
-                moveList.add(new ChessMove(myPosition, new ChessPosition(newRow, newCol), null));
+            if (limited) {
+                isValid = 0;
             }
+            counter += 1;
         }
     }
 }
