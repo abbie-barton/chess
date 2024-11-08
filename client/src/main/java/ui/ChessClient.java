18360d5db395dc0cb46dc462d801959808a8283f
Abbie Barton <abbie.s.barton@gmail.com>
2024-11-07 20:53:22 -0700

    create starter repl files

---
+++ client/src/main/java/ui/ChessClient.java
@@ -0,0 +1,7 @@
+package ui;
+
+public class ChessClient {
+    public ChessClient() {
+        System.out.println("Inside chessClient constructor");
+    }
+}
---
+++ client/src/main/java/ui/Repl.java
@@ -0,0 +1,20 @@
+package ui;
+
+import java.util.Scanner;
+
+public class Repl {
+    private final ChessClient client;
+
+    public Repl(String serverUrl) {
+        client = new ChessClient();
+    }
+
+    public void run() {
+        System.out.println("\uD83D\uDC36 Let's play chess! Sign in to start.");
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+
+        System.out.println();
+    }
+}
