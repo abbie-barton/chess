7b86a5ddc847f1a8ce14790fd0261b581146e93a
Abbie Barton <abbie.s.barton@gmail.com>
2024-11-14 12:40:08 -0700

    next batch of tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,15 +1,21 @@
 package client;
 
+import chess.ChessGame;
+import dataaccess.DataAccess;
+import dataaccess.MySqlDataAccess;
 import model.*;
 import org.junit.jupiter.api.*;
 import server.Server;
 import ui.ServerFacade;
+
+import java.util.List;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
     private static ServerFacade serverFacade;
+    static private DataAccess dataAccess;
 
     @BeforeAll
     public static void init() {
@@ -17,6 +23,7 @@ public class ServerFacadeTests {
         var port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
         serverFacade = new ServerFacade("http://localhost:8080");
+        dataAccess = new MySqlDataAccess();
     }
 
     @AfterEach
@@ -49,6 +56,83 @@ public class ServerFacadeTests {
         UserData newUser = new UserData(null, null, null);
         try {
             AuthData returnedAuth = serverFacade.createUser(newUser);
+            Assertions.fail();
+        } catch (Exception ex) {
+            Assertions.assertNotNull(ex);
+        }
+    }
+
+    @Test
+    public void positiveLogin() {
+        UserData newUser = new UserData("b", "b", "b");
+        try {
+            AuthData returnedAuth = serverFacade.createUser(newUser);
+            AuthData loginAuth = serverFacade.login(newUser);
+            Assertions.assertNotNull(loginAuth);
+        } catch (Exception ex) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    public void negativeLogin() {
+        UserData newUser = new UserData("b", "b", "b");
+        try {
+            serverFacade.createUser(newUser);
+            serverFacade.login(new UserData("7", "s", "f"));
+            Assertions.fail();
+        } catch (Exception ex) {
+            Assertions.assertNotNull(ex);
+        }
+    }
+
+    @Test
+    public void positiveLogout() {
+        UserData newUser = new UserData("c", "c", "c");
+        try {
+            serverFacade.createUser(newUser);
+            AuthData loginAuth = serverFacade.login(newUser);
+            serverFacade.logout(loginAuth.authToken());
+            Assertions.assertNull(dataAccess.getAuth(loginAuth.authToken()));
+        } catch (Exception ex) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    public void negativeLogout() {
+        UserData newUser = new UserData("d", "d", "d");
+        try {
+            serverFacade.createUser(newUser);
+            serverFacade.logout("fake auth token");
+            Assertions.fail();
+        } catch (Exception ex) {
+            Assertions.assertNotNull(ex);
+        }
+    }
+
+    @Test
+    public void positiveListGames() {
+        UserData newUser = new UserData("e", "e", "e");
+        try {
+            AuthData loginAuth = serverFacade.createUser(newUser);
+            GameData newGame = new GameData(1, null, null, "newGame", new ChessGame());
+            serverFacade.createGame(loginAuth.authToken(), newGame);
+            List<GameData> games = serverFacade.listGames(loginAuth.authToken());
+            Assertions.assertNotNull(games);
+        } catch (Exception ex) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    public void negativeListGames() {
+        UserData newUser = new UserData("e", "e", "e");
+        try {
+            AuthData loginAuth = serverFacade.createUser(newUser);
+            GameData newGame = new GameData(1, null, null, "newGame", new ChessGame());
+            serverFacade.createGame(loginAuth.authToken(), newGame);
+            serverFacade.listGames("fake auth token");
             Assertions.fail();
         } catch (Exception ex) {
             Assertions.assertNotNull(ex);
